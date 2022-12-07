e8c9ec0b78e7af512b5c150cd7710d192d55c545
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-12-07 18:52:45 +1100

    change info on each page and switch next button text to submit button text

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ const step1 = document.querySelector(".step-1");
 const two = document.querySelector(".two");
 const three = document.querySelector(".three");
 const four = document.querySelector(".four");
+
+const step2 = document.querySelector(".step-2");
+const step3 = document.querySelector(".step-3");
+const step4 = document.querySelector(".step-4");
+const step5 = document.querySelector(".step-5");
+step2.style.display = "none";
+step3.style.display = "none";
+step4.style.display = "none";
+step5.style.display = "none";
+
 nextButton.addEventListener("click", () => {
     // array.forEach(value => {
 
@@ -21,16 +31,24 @@ nextButton.addEventListener("click", () => {
         one.classList.toggle("active");
         two.classList.toggle("active");
         step1.style.display = "none";
+        step2.style.display = "block";
     } else if (two.classList.contains("active")) {
         two.classList.toggle("active");
         three.classList.toggle("active");
+        step2.style.display = "none";
+        step3.style.display = "block";
     } else if (three.classList.contains("active")) {
         three.classList.toggle("active");
         four.classList.toggle("active");
+        step3.style.display = "none";
+        step4.style.display = "block";
+        nextButton.textContent = "Submit";
     } else if (four.classList.contains("active")) {
-        four.classList.toggle("active");
-        one.classList.toggle("active");
-        step1.style.display = "block";
+        // four.classList.toggle("active");
+        // one.classList.toggle("active");
+        nextButton.style.display = "none";
+        step4.style.display = "none";
+        step5.style.display = "block";
     } 
 
     
