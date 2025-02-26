2f40c25613abb52d779094873825e5281d1d355d
Abdullah Yakub <abdullah.yakub@hotmail.com>
2025-02-27 01:21:52 +1100

    fix styling and functionality of step 3 on desktop. Add plan switching on step 2. save input to an object

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const subscription = {
+  plan: {
+    planType: "Arcade",
+    price: "$9/mo",
+  },
+  addOns: []
+}
+
+/*
+const addOnObj = {
+  addOnName: "",
+  price: -1
+}
+*/
+
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
 const three = document.querySelector(".three");
@@ -9,6 +24,7 @@ const step3 = document.querySelector(".step-3");
 const step4 = document.querySelector(".step-4");
 const step5 = document.querySelector(".step-5");
 
+// steps behaviour
 const steps = [step1, step2, step3, step4, step5];
 steps.slice(1, steps.length).forEach(s => s.style.display = "none");
 steps.slice(0, steps.length - 1).forEach(s => {
@@ -27,6 +43,35 @@ steps.slice(0, steps.length - 1).forEach(s => {
 
 step1.querySelector(".buttons").querySelector(".prev").style.visibility = "hidden";
 step4.querySelector(".next").textContent = "Submit";
+const planCards = document.querySelectorAll(".card");
+const planCardsArr = Array.from(planCards);
+
+
+const addOns = document.querySelectorAll(".add-on");
+const addOnsArr = Array.from(addOns);
+const updatePlan = () => {
+  const planCard = planCardsArr.find(pC => pC.classList.contains("card-active"));
+  const cardTextContainer = planCard.querySelector("div");
+  const cardTextH = cardTextContainer.querySelector("h3");
+  const cardTextP = cardTextContainer.querySelector("p");
+  subscription.plan.planType = cardTextH.textContent;
+  subscription.plan.price = cardTextP.textContent;
+}
+
+const updateAddOns = () => {
+  const selectedAddOns = addOnsArr
+    .filter(addOn => addOn.classList.contains("card-active"))
+    .map(addOn => {
+      const cardTextContainer = addOn.querySelector("div");
+      const addOnName = cardTextContainer.querySelector("h3").textContent;
+      const price = addOn.querySelectorAll("p")[1].textContent;
+      return { addOnName, price }
+    });
+  subscription.addOns = selectedAddOns;
+
+
+
+}
 
 const nextState = (item) => {
   step1.style.display = "none";
@@ -49,10 +94,12 @@ const nextState = (item) => {
       two.classList.add("active");
       break;
     case "three":
+      updatePlan();
       step3.style.display = "flex";
       three.classList.add("active");
       break;
     case "four":
+      updateAddOns();
       step4.style.display = "flex";
       four.classList.add("active");
       break;
@@ -98,16 +145,16 @@ step4.querySelector(".prev").addEventListener("click", () => {
   nextState("three");
 });
 
+// yearly and monthly behaviour
 const timeSwitch = document.querySelector(".time-period").querySelector("span");
 const timeSwitchCircle = timeSwitch.querySelector("span");
-const planCards = document.querySelectorAll(".card");
+
 
 const handleMonthlySwitch = (planCard) => {
   const cardTextContainer = planCard.querySelector("div");
   const cardTextH = cardTextContainer.querySelector("h3");
   const cardTextP = cardTextContainer.querySelector("p");
   cardTextContainer.removeChild(cardTextP);
-  console.log(cardTextH.textContent === "Arcade")
   switch (cardTextH.textContent) {
     case "Arcade":
       cardTextP.textContent = "$9/mo";
@@ -159,6 +206,53 @@ const handleYearlySwitch = (planCard) => {
 
 }
 
+const handleAddOnMonthlySwitch = (addOn) => {
+  const cardTextContainer = addOn.querySelector("div");
+  const cardTextH = cardTextContainer.querySelector("h3");
+  const cardTextP = addOn.querySelectorAll("p")[1];
+  addOn.removeChild(cardTextP);
+  switch (cardTextH.textContent) {
+    case "Online service":
+      cardTextP.textContent = "+$1/mo";
+      break;
+    case "Larger storage":
+      cardTextP.textContent = "+$2/mo";
+      break;
+    case "Customizable profile":
+      cardTextP.textContent = "+$2/mo";
+      break;
+
+    default:
+      break;
+  }
+
+  addOn.appendChild(cardTextP);
+}
+
+
+const handleAddOnYearlySwitch = (addOn) => {
+  const cardTextContainer = addOn.querySelector("div");
+  const cardTextH = cardTextContainer.querySelector("h3");
+  const cardTextP = addOn.querySelectorAll("p")[1];
+  addOn.removeChild(cardTextP);
+  switch (cardTextH.textContent) {
+    case "Online service":
+      cardTextP.textContent = "+$10/yr";
+      break;
+    case "Larger storage":
+      cardTextP.textContent = "+$20/yr";
+      break;
+    case "Customizable profile":
+      cardTextP.textContent = "+$20/yr";
+      break;
+
+    default:
+      break;
+  }
+
+  addOn.appendChild(cardTextP);
+}
+
 const handleTimeSwitch = () => {
   timeSwitchCircle.classList.toggle("yearly-switch");
   planCards.forEach(planCard => {
@@ -166,6 +260,14 @@ const handleTimeSwitch = () => {
       handleYearlySwitch(planCard);
     } else {
       handleMonthlySwitch(planCard);
+    }
+  });
+
+  addOns.forEach(addOn => {
+    if (timeSwitchCircle.classList.contains("yearly-switch")) {
+      handleAddOnYearlySwitch(addOn)
+    } else {
+      handleAddOnMonthlySwitch(addOn)
     }
   })
 
@@ -175,3 +277,33 @@ const handleTimeSwitch = () => {
 
 
 timeSwitch.addEventListener("click", handleTimeSwitch);
+
+// plan selection behavior 
+planCards.forEach(planCard => {
+  planCard.addEventListener("click", () => {
+    planCardsArr
+      .filter(pC => pC.classList.contains("card-active"))
+      .forEach(pC => {
+        pC.classList.remove("card-active")
+
+      });
+
+    planCard.classList.add("card-active");
+  })
+})
+
+// check box add on behaviour
+
+addOns.forEach(addOn => {
+  addOn.addEventListener("click", () => {
+    const checkBox = addOn.querySelector("input");
+    checkBox.checked = checkBox.checked ? false : true;
+    const isChecked = checkBox.checked;
+    if (isChecked) {
+      addOn.classList.add("card-active")
+    } else {
+      addOn.classList.remove("card-active")
+    }
+
+  })
+})
